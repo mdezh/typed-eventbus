fe6214e2f7a69f56cf54008442635c6d08fa9d3b
mikhail <m.m.dezhurko@gmail.com>
2022-02-20 03:31:08 +0300

    Use _ as prefix for private members that should be mangled

--- src/eventbus.ts
+++ src/eventbus.ts
@@ -1,34 +1,32 @@
-type EventMap = {
+export type EventMap = {
   [eventName: string]: (...args: any[]) => void;
 };
 
 export class EventBus<T extends EventMap> {
-  private eventListeners: Map<keyof T, Set<T[keyof T]>> = new Map();
+  private _eventListeners: Map<keyof T, Set<T[keyof T]>> = new Map();
 
   publish<E extends keyof T>(eventName: E, ...args: Parameters<T[E]>): void {
-    const listeners = this.eventListeners.get(eventName);
+    const listeners = this._eventListeners.get(eventName);
     if (!listeners) return;
 
     listeners.forEach((listener) => listener(...args));
   }
 
   subscribe<E extends keyof T>(eventName: E, listener: T[E]): () => void {
-    if (!this.eventListeners.has(eventName)) {
-      this.eventListeners.set(eventName, new Set());
+    if (!this._eventListeners.has(eventName)) {
+      this._eventListeners.set(eventName, new Set());
     }
-    const listeners = this.eventListeners.get(eventName)!;
+    const listeners = this._eventListeners.get(eventName)!;
     listeners.add(listener);
 
     return () => {
-      const listeners = this.eventListeners.get(eventName);
+      const listeners = this._eventListeners.get(eventName);
       if (!listeners?.has(listener)) return;
 
       listeners.delete(listener);
       if (listeners.size === 0) {
-        this.eventListeners.delete(eventName);
+        this._eventListeners.delete(eventName);
       }
     };
   }
 }
-
-export default EventBus;
